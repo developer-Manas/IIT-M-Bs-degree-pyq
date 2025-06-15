21e375d977948b538deeed7f7c7899532e613b68
developer-Manas <Manaskumar9102@gmail.com>
2025-06-15 09:14:04 +0530

    .

--- statistics/2-questions.js
+++ statistics/2-questions.js
@@ -1,6 +1,28 @@
 let questions = [
     {
         numb: 1,
+        question: "A cab driver got the rating as feedback from 20 customers on a given day which are as follows: 7 9 8 x 6 9 7 5 9 10 7 8 9 8 8 7 10 6 6 9....  If the relative frequency of rating 7 is 0.25, then what is the value of x?",
+        answer: "A. 7",
+        options: [
+            "A. 7",
+            "B. 8",
+            "C. 9",
+            "D. Data insufficient"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A cab driver got the rating as feedback from 20 customers on a given day which are as follows: 7 9 8 x 6 9 7 5 9 10 7 8 9 8 8 7 10 6 6 9....  What is the relative frequency of rating 8?",
+        answer: "A. 0.2",
+        options: [
+            "A. 0.2",
+            "B. 0.25",
+            "C. 0.2",
+            "D. 0.15"
+        ]
+    },
+    {
+        numb: 3,
         question: "",
         answer: "",
         options: [
@@ -11,7 +33,7 @@ let questions = [
         ]
     },
     {
-        numb: 1,
+        numb: 4,
         question: "",
         answer: "",
         options: [
@@ -22,29 +44,7 @@ let questions = [
         ]
     },
     {
-        numb: 1,
-        question: "",
-        answer: "",
-        options: [
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
-        ]
-    },
-    {
-        numb: 1,
-        question: "",
-        answer: "",
-        options: [
-            "A. ",
-            "B. ",
-            "C. ",
-            "D. "
-        ]
-    },
-    {
-        numb: 1,
+        numb: 5,
         question: "",
         answer: "",
         options: [
