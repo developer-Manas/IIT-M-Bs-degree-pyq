34a51469b339ed29e1114454d5bc55c824d94220
developer-Manas <Manaskumar9102@gmail.com>
2025-06-15 06:17:21 +0530

    just an small update

---
+++ ct/2-questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: " At any intermediate stage in the process of moving the cards from one pile to the other, what does the value in the variable count represent?",
+        answer: "B. It is the number of cards that have been moved from one pile to the other so far",
+        options: [
+            "A. It does not mean anything",
+            "B. It is the number of cards that have been moved from one pile to the other so far",
+            "C. ",
+            "D. It is the number of cards that are remaining in the first pile"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Suppose we wish to find the shortest sentence in the “Paragraph words” dataset. We decide to use two variables, min and count that keep track of the length of the shortest sentence seen so far and the length of the current sentence respectively. Which of the following is a valid initialisation for min?",
+        answer: "A. 0",
+        options: [
+            "A. 0",
+            "B. 3",
+            "C. 1000",
+            "D. 400"
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+];
