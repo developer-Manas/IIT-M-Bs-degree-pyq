5d7d170f6fdba40319484a4a2760f2c581e446f0
developer-Manas <Manaskumar9102@gmail.com>
2025-06-15 05:40:08 +0530

    Questions for maaths week 2 updated

---
+++ mathematics/2-questions.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Choose the correct option with respect to the points P(5,-3), Q(-3,3), R(0,-100), and S(-2.5,0) on the rectangular coordinate system.",
+        answer: "Point R does not lie in any quadrant.",
+        options: [
+            "A. Point R does not lie in any quadrant.",
+            "B. Point P and R lie in quadrant III",
+            "C. Point S and Q lie in Quadrant II",
+            "D. Point R and S cannot be represented on the rectangular coordinate system"
+        ]
+    },
+     {
+        numb: 2,
+        question: " Which of the following is/are correct with respect to the rectangular coordinate system?",
+        answer: "B. The point of intersection of the X and Y axes is called the origin",
+        options: [
+            "A. The horizontal line is called Y-axis",
+            "B. The point of intersection of the X and Y axes is called the origin",
+            "C. The vertical line is called X-axis",
+            " "
+        ]
+    },
+    {
+        numb: 3,
+        question: "The distance of a point P(1, |tan(θ)|) from the origin is _______ (Hint: |tan²(θ)| + 1 = |sec²(θ)|)",
+        answer: "B. |sec(θ)|",
+        options: [
+            "A. 1",
+            "B. |sec(θ)|",
+            "C. |tan(θ)|",
+            "D. -cosec(θ)"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The distance of a point lying on the X-coordinate from the origin is _______",
+        answer: "",
+        options: [
+            "A. The value of the vertical distance of that point from the origin",
+            "B. Cannot be determined",
+            "C. The value of the horizontal distance of that point from the origin",
+            "D. 0"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The distance between P(-5,-2) and Q(-2,2) is____",
+        answer: "D. 5",
+        options: [
+            "A. 7",
+            "B. 9",
+            "C. -6",
+            "D. 5"
+        ]
+    },
+
+
+
+];
---
+++ mathematics/questions.js
@@ -0,0 +1,13 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: ["C. Hyper Text Markup Language" , "D. Home Text Multi Language"],
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    }
+];
