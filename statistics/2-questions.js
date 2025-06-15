8540138f1825860d98680cebc044d56af871b5e9
developer-Manas <Manaskumar9102@gmail.com>
2025-06-15 06:58:40 +0530

    hey

---
+++ statistics/2-questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+     
+
+
+
+];
